d41034d3c3e3ed860db8ebe4b1f9b48f5c871052
elevatic20 <elevatic20@foi.hr>
2023-12-06 15:32:20 +0100

    v1.1

--- csv-reader-app/src/App.tsx
+++ csv-reader-app/src/App.tsx
@@ -16,8 +16,32 @@ const CsvUploader: React.FC = () => {
     for (const file of acceptedFiles) {
       // Provera ekstenzije .csv
       if (file.name.endsWith(".csv")) {
-        const result = await readCsvFile(file);
-        tempCurrencySums = calculateCurrencySums(tempCurrencySums, result.data);
+        try {
+          const result = await readCsvFile(file);
+
+          if (typeof result === "object" && result !== null) {
+            if (
+              "errors" in result &&
+              result.errors &&
+              result.errors.length > 0
+            ) {
+              console.error(`Error parsing CSV file: ${file.name}`);
+              console.error(result.errors);
+            } else if ("data" in result && result.data) {
+              tempCurrencySums = calculateCurrencySums(
+                tempCurrencySums,
+                result.data
+              );
+            } else {
+              console.error(`No data found in CSV file: ${file.name}`);
+            }
+          } else {
+            console.error(`Unexpected result type for CSV file: ${file.name}`);
+          }
+        } catch (error) {
+          console.error(`Error reading CSV file: ${file.name}`);
+          console.error(error);
+        }
       } else {
         console.error(`Unsupported file format: ${file.name}`);
       }
@@ -27,10 +51,13 @@ const CsvUploader: React.FC = () => {
   };
 
   const readCsvFile = async (file: File) => {
-    return new Promise((resolve) => {
+    return new Promise<any>((resolve) => {
       Papa.parse(file, {
         complete: (result) => {
           resolve(result);
+        },
+        error: (error) => {
+          resolve({ errors: [error] });
         },
       });
     });
@@ -65,14 +92,40 @@ const CsvUploader: React.FC = () => {
     multiple: true,
   });
 
+  //Pocetak "HTML-a"
+
   return (
     <div>
+      <h1
+        style={{
+          marginBottom: "20px",
+          fontSize: "40px",
+          background: "#f10000",
+          margin: "0 auto",
+          paddingTop: "10px",
+          paddingBottom: "20px",
+          width: "100%",
+        }}
+      >
+        CSV currency reader
+      </h1>
+      <p style={{ fontSize: "20px" }}>
+        Ovo je privatni projekt razvijen od strane <b>Emanuela Levatića</b>.
+        <br />
+        Koji omogućava da se odabere <b>određen</b> broj <b>".csv"</b> datoteka
+        te da se izračuna suma po valutama. <br /> Korišteni su:
+        <b> React i TypeScript</b>.
+      </p>
       <div
+        className="draganddrop"
         {...getRootProps()}
         style={{
           border: "1px solid #ccc",
           padding: "10px",
           borderRadius: "4px",
+          width: "25%",
+          margin: "0 auto",
+          fontSize: "17px",
         }}
       >
         <input {...getInputProps()} />
@@ -81,7 +134,7 @@ const CsvUploader: React.FC = () => {
       {currencySums.length > 0 && (
         <div>
           <h2>Total Sums by Currency:</h2>
-          <ul>
+          <ul style={{ listStyle: "none" }}>
             {currencySums.map((currencyData) => (
               <li key={currencyData.currency}>
                 {currencyData.currency}: {currencyData.sum}
